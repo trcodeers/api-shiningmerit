6079bbd22997be6b362607f113098b849ffe1e8b
Tarun-kumar-singh <tarunks2512@gmail.com>
2022-08-24 19:51:16 +0530

    change table schema

--- src/models/table.js
+++ src/models/table.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 
-const tableSchema = new mongoose.Schema({}, { strict: false });
+const tableSchema = new mongoose.Schema({
+    textToSearch: {
+        type: String,
+        required: true,
+        trim: true
+    }
+}, { strict: false });
 
 const Table = mongoose.model('Tables', tableSchema);
 
