1416c8b72685130c85448af30ad42d786eaedb9e
Tarun-kumar-singh <tarunks2512@gmail.com>
2022-09-09 14:49:24 +0530

    error

--- src/validations/mcq.js
+++ src/validations/mcq.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 function MCQValidate(data) {
     const mcq = Joi.object({
          question: Joi.string().required(),
-         options: Joi.array().length(4).required,
+         options: Joi.array().length(4).required(),
          rightAnswer: Joi.number().required(),
          tags: Joi.array(),
       })  
