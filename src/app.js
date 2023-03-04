690cbe0f7d7efe7acb7e6cfb24ddfd2060776073
Tarun-kumar-singh <tarunks2512@gmail.com>
2023-03-05 02:14:23 +0530

    add whitelist

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 
 });
 
-const whiteList = ['http://localhost:3000', 'https://admin-shiningmerit.herokuapp.com', 'https://user-shiningmerit.herokuapp.com', 'https://shiningmerit.com']
+const whiteList = ['http://localhost:3000','https://user-shiningmerit.up.railway.app', 'https://admin-shiningmerit.herokuapp.com', 'https://user-shiningmerit.herokuapp.com', 'https://shiningmerit.com']
 
 app.use(cors({
     origin: whiteList
